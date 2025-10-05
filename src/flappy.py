e0176dd4e5b5375f8c15ea2e3f8e0bb12c32a8ee
Jaebaek Seo <jaebaek@google.com>
2025-10-05 04:10:14 +0000

    Start game right away and restart on death

--- src/flappy.py
+++ src/flappy.py
@@ -43,9 +43,9 @@ class Flappy:
             self.game_over_message = GameOver(self.config)
             self.pipes = Pipes(self.config)
             self.score = Score(self.config)
-            await self.splash()
+            # await self.splash()
             await self.play()
-            await self.game_over()
+            # await self.game_over()
 
     async def splash(self):
         """Shows welcome splash screen animation of flappy bird"""
